cb8c87a7f6796600da8b5cfb497ccefc1ad13ee1
Security-Arts <71318020+Security-Arts@users.noreply.github.com>
2025-12-07 21:48:21 +0200

    Update page.tsx

--- app/hive/page.tsx
+++ app/hive/page.tsx
@@ -2,7 +2,8 @@
 
 import React, { useEffect, useState } from "react";
 import Link from "next/link";
-
+import { HiveTable } from "@/components/HiveTable";
+<HiveTable ideas={visibleIdeas} onPulse={handlePulseChange} />
 type Idea = {
   id: string;
   title: string;
